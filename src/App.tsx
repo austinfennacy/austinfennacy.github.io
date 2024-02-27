807f0dd202f83642cca4ec30e8d3747c956bd427
Austin Fennacy <33964526+austinfennacy@users.noreply.github.com>
2024-02-26 22:16:36 -0800

    :wrench: Change page title.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Button from 'react-bootstrap/Button';
